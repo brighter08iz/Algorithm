84b5e6300825e82c53d21cb68f58e57835ece64c
Soyeon Park <151604861+brighter08iz@users.noreply.github.com>
2023-11-27 23:54:54 +0900

    [Bronze V] Title: 꼬마 정민, Time: 212 ms, Memory: 17636 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Main T = new Main();
+        Scanner scanner = new Scanner(System.in);
+
+        Long a = scanner.nextLong();
+        Long b = scanner.nextLong();
+        Long c = scanner.nextLong();
+        System.out.print(a + b + c);
+    }
+}
